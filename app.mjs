3f0a5a7631f4f800665dfd376576783ee0a55b27
Sirawit-Boon <ter.sirawit1997@gmail.com>
2024-11-07 13:25:15 +0700

    get: data from user

--- app.mjs
+++ app.mjs
@@ -7,14 +7,13 @@ const port = process.env.PORT || 4001;
 app.use(cors());
 app.use(express.json());
 
-const getData = {
-  data: {
-    name: "john",
-    age: 20,
-  },
-};
 app.get("/profiles", (req, res) => {
-  return res.json(getData);
+  return res.json({
+    data: {
+      name: "john",
+      age: 20,
+    },
+  });
 });
 
 app.listen(port, () => {
