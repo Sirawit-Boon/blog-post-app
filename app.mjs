0746599cf7d59fafc03e24553dd5fdbf396454e8
Sirawit-Boon <ter.sirawit1997@gmail.com>
2024-11-07 13:49:33 +0700

    fix app.mjs

--- app.mjs
+++ app.mjs
@@ -2,21 +2,17 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 4001;
 
 app.use(cors());
 app.use(express.json());
 
-const getData = {
-  data: {
-    name: "john",
-    age: 20,
-  },
-};
 app.get("/profiles", (req, res) => {
-  return res.json(getData);
+  return res.json({
+    data: {
+      name: "john",
+      age: 20,
+    },
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
-});
+export default app;
